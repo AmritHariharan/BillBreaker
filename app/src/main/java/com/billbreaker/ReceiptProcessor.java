fa4e99b109e914719246474fa3310b18a7abaaa9
Amrit Hariharan <t-amhari@microsoft.com>
2019-07-22 14:07:36 -0700

    added request code from msft docs

--- app/src/main/java/com/billbreaker/ReceiptProcessor.java
+++ app/src/main/java/com/billbreaker/ReceiptProcessor.java
@@ -2,30 +2,52 @@ package com.billbreaker;
 
 import android.media.Image;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 class ReceiptProcessor {
 
-    static void getItemsFromImage(Image receipt) { // FIXME: actually return something lmao
-        HttpURLConnection conn = null;
+    private static void getItemsFromImage(Image receipt) { // FIXME: actually return something lmao
+        HttpClient httpclient = HttpClients.createDefault();
         try {
-            URL u = new URL("https://westus.api.cognitive.microsoft.com/vision/v2.0/ocr?language=unk&detectOrientation=true");
-            conn = (HttpURLConnection) u.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/octet-stream");
-            conn.setRequestProperty("Ocp-Apim-Subscription-Key", BuildConfig.AZURE_COGNITION_KEY);
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/vision/v2.0/ocr");
+
+            // Request header
+            builder.setParameter("language", "unk");
+            builder.setParameter("detectOrientation", "true");
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", BuildConfig.AZURE_COGNITION_KEY);
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{body}");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     static List<ReceiptItem> processItems(Image receipt) {
         getItemsFromImage(receipt);
+        return new ArrayList<ReceiptItem>();
     }
 }
